2f9ecd8a82b5bccc2949e95267ecc986a97573c7
Marfa <61018185+marfachornaya47@users.noreply.github.com>
2024-06-15 20:14:51 +1000

    Add files via upload

---
+++ fire/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+class beng
+{
+    static void Main()
+    {
+        Console.Write("Введите количество бенгальских огней у Игоря: ");
+        int c1 = int.Parse(Console.ReadLine());
+
+        int b1 = 2;
+
+        int time = 2, allFire = 0;
+
+        if (c1 > 0)
+        {
+            allFire = (c1 + b1) * time;
+        }
+
+        Console.WriteLine($"Огни будут гореть в часах: {allFire}");
+        Console.ReadKey();
+    }
+}
